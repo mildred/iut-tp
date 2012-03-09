7f73e3a2a76848a4b8333343e60166dc6f7361d1
BOUCHEZ-MONGARDE Shanti <bouchezs@iut2.upmf-grenoble.fr>
2012-03-09 09:01:49 +0100

    Update p_photo with prix

--- tp17/p_photo.adb
+++ tp17/p_photo.adb
@@ -90,5 +90,31 @@ package body P_Photo is
     end loop;
     l := liste2;
   end trilisteprix;
+ 
+  ------------------------------------------------------------------------------
+  -- POUR LES EXTENSIONS
+  ------------------------------------------------------------------------------
+  function Gammedeprix(V : in TV_EchellePrix ; P : in Positive) return T_Gamme is
+    -- {} => {resulat = gamme de prix correspondante au prix P selon l'echelle V}
+    I : T_Gamme;
+  begin
+    I := V'First;
+    while I<V'Last and P >= V(I) loop
+      I:= T_Gamme'Succ(I);
+    end loop;
+    return I;
+  end Gammedeprix;  
+  
+  procedure IntervalledeGamme(V : in TV_EchellePrix ; Gamme :in T_Gamme; Binf, Bsup : out Positive) is
+    -- {} => {Binf = Borne de prix inf pour la gamme Gamme, Bsup = Borne de prix
+    --        sup pour la gamme Gamme, bornes incluses}
+  begin
+    Bsup := V(Gamme) - 1;
+    if Gamme = V'First then 
+      Binf  := 1;
+    else 
+      Binf := V(T_Gamme'Pred(Gamme));
+    end if;
+  end Intervalledegamme;  
 
 end P_photo;
